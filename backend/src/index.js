a5b472c6ccd6c621e0ff879ef20e8f5da22083bc
tienvm <tienvm@bytetech.io>
2026-07-11 18:34:20 +0700

    Add user and message models, connect to MongoDB, and integrate Clerk middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,23 @@
+import { clerkMiddleware } from "@clerk/express";
+import "dotenv/config";
 import express from "express";
+import cors from "cors";
+import { connectDB } from "./lib/db.js";
+
+const app = express();
+const PORT = process.env.PORT;
+const FRONTEND_URL = process.env.FRONTEND_URL;
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: FRONTEND_URL,
+    credentials: true,
+  }),
+);
+app.use(clerkMiddleware());
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running on port ${PORT}`);
+});
